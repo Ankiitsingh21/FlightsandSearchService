3e76d58fff7c46bd48327ab15a9571f0f1b341ac
Ankit Singh <ankiitsingh21@gmail.com>
2024-01-15 22:51:51 +0530

    Added the service laye - city-services.js

---
+++ src/services/city-service.js
@@ -0,0 +1,48 @@
+const {CityRepository} = require('../repository/index');
+
+class CityService{
+        constructor(){
+                this.CityRepository=new CityRepository();
+        }
+        async createCity(){
+            try{
+               const city = await this.CityRepository.createCity(data);
+               return city;
+            }
+            catch(error){
+                console.log("something went wrong on service layer");
+                throw {error};
+            }
+        }
+        async deleteCity(cityId){
+                try{
+                  const response = await this.CityRepository.deletecity(cityId);
+                  return response;
+                }
+                catch(error){
+                        console.log("something went wrong on service layer");
+                        throw {error};
+                }
+        }
+        async updateCity(cityId,data){
+                try{
+                 const city = await this.CityRepository.updateCity(cityId,data);
+                 return city;
+                }
+                catch(error){
+                        console.log("something went wrong on service layer");
+                        throw {error};
+                }
+        }
+        async getCity(cityId){
+                try{
+                  const city = await this.CityRepository.getCity(cityId);
+                  return city;
+                }
+                catch(error){
+                        console.log("something went wrong on service layer");
+                        throw {error};
+                }
+        }
+}
+module.exports = CityService;
