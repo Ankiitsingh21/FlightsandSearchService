f00906b33d195b7a5b25fecdec640ff42f515a17
Ankit Singh <ankiitsingh21@gmail.com>
2024-01-15 22:18:51 +0530

    updated city repository.js

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -7,19 +7,46 @@ class CityRepository{
               return city;
          }
          catch(error){
+            console.log("Something wwent wrong in the repository layer");
             throw {error};
          }
         }
-        async deletecity( id ){
+        async deletecity( cityid ){
                 try{
                      await City.destroy({
                         where:{
                                 id:cityid
                         }
                      });
+                     return true;
                 }
                 catch(error){
+                        console.log("Something wwent wrong in the repository layer");
                         throw {error};
+                }
+        }
+        async updateCity(cityId, data){
+              try{
+                   const city = await City.update(data,{
+                         where:{
+                                id:cityId
+                         }
+                   });
+                   return city;
+              }
+              catch(error){
+                    console.log("Something wwent wrong in the repository layer");
+                        throw {error};
+              }
+        }
+        async getCity(cityid){
+                try{
+                     const city = await City.findByPK(cityid);
+                     return city;
+                }
+                catch(error){
+                        console.log("Something wwent wrong in the repository layer");
+                        throw {error};   
                 }
         }
 }
