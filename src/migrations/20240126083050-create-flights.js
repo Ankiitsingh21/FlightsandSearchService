7410473e1da23daf48c30a1530fa2e2f0b3cd7b2
Ankit Singh <ankiitsingh21@gmail.com>
2024-01-26 14:14:58 +0530

    Added Flights model

---
+++ src/migrations/20240126083050-create-flights.js
@@ -0,0 +1,61 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Flights', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      filghtNumber: {
+        type: Sequelize.STRING,
+        allowNull:false,
+        unique:true,
+      },
+      airplaneId: {
+        type: Sequelize.INTEGER,
+        allowNull:false
+      },
+      depatureAirportId: {
+        type: Sequelize.INTEGER,
+        allowNull:false
+      },
+      arrivalAirportId: {
+        type: Sequelize.INTEGER,
+        allowNull:false
+      },
+      arrivalTime: {
+        type: Sequelize.DATE,
+        allowNull:false
+      },
+      depatureTime: {
+        type: Sequelize.DATE,
+        allowNull:false
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull:false
+      },
+      boardingGate: {
+        type: Sequelize.STRING
+      },
+      totalSeats: {
+        type: Sequelize.INTEGER,
+        allowNull:false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Flights');
+  }
+};
